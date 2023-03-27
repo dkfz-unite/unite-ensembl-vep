03ef59bbe1bcaea9cc07c37280662c4887e47394
vulasau <valiantsin.ulasau@gmail.com>
2023-03-27 17:33:41 +0200

    Update VepFile.cs

--- Ensembl.Vep.Web/Services/VepFile.cs
+++ Ensembl.Vep.Web/Services/VepFile.cs
@@ -6,7 +6,8 @@ namespace Ensembl.Vep.Web.Services
 {
     public abstract class VepFile : IDisposable
     {
-        private const string _cacheFolder = "/opt/vep/.vep";
+        private const string _cacheFolder = "/data";
+        // private const string _cacheFolder = "/opt/vep/.vep";
         // private const string _cacheFolder = "/app/data";
 
 
